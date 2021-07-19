f04a9d654ca18c6c332919b9b6dad9b7160ee61a
NKrody <1304473651@qq.com>
2021-07-19 23:40:23 +0800

    装饰器练习

---
+++ Decorator.py
@@ -0,0 +1,53 @@
+# 在面向对象（OOP）的设计模式中，decorator被称为装饰模式。
+# OOP的装饰模式需要通过继承和组合来实现，而Python除了能支持OOP的decorator外，直接从语法层次支持decorator。
+# Python的decorator可以用函数实现，也可以用类实现。
+
+# import functools
+
+# 模板：
+
+# def log(func):
+#     @functools.wraps(func)
+#     def wrapper(*args, **kw):
+#         print('call %s():' % func.__name__)
+#         return func(*args, **kw)
+#     return wrapper
+
+# 针对带参的decorator:
+
+# def log(text):
+#     def decorator(func):
+#         @functools.wraps(func)
+#         def wrapper(*args, **kw):
+#             print('%s %s():' % (text, func.__name__))
+#             return func(*args, **kw)
+#         return wrapper
+#     return decorator
+
+# 请设计一个decorator，它可作用于任何函数上，并打印该函数的执行时间：
+import time, functools
+
+def metric(fn):
+    @functools.wraps(fn)
+    def wrapper(*args, **kw):
+        print('%s executed in %s ms' % (fn.__name__, 10.24))
+        return fn(*args, **kw)
+    return wrapper
+
+# 测试
+@metric
+def fast(x, y):
+    time.sleep(0.0012)
+    return x + y
+
+@metric
+def slow(x, y, z):
+    time.sleep(0.1234)
+    return x * y * z
+
+f = fast(11, 22)
+s = slow(11, 22, 33)
+if f != 33:
+    print('测试失败!')
+elif s != 7986:
+    print('测试失败!')
